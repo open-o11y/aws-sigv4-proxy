244423174598ccebe81a53e5f0fa0b6ac6bbe131
Aman Brar <mabrara@amazon.com>
2020-11-23 12:08:18 -0500

    removed signature logic

--- handler/proxy_client.go
+++ handler/proxy_client.go
@@ -16,15 +16,13 @@
 package handler
 
 import (
-	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws/endpoints"
-	"github.com/aws/aws-sdk-go/aws/signer/v4"
+	v4 "github.com/aws/aws-sdk-go/aws/signer/v4"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -35,44 +33,16 @@ type Client interface {
 
 // ProxyClient implements the Client interface
 type ProxyClient struct {
-	Signer *v4.Signer
-	Client Client
+	Signer              *v4.Signer
+	Client              Client
 	StripRequestHeaders []string
 	SigningNameOverride string
-	HostOverride string
-	RegionOverride string
+	HostOverride        string
+	RegionOverride      string
 }
 
 func (p *ProxyClient) sign(req *http.Request, service *endpoints.ResolvedEndpoint) error {
-	body := bytes.NewReader([]byte{})
-
-	if req.Body != nil {
-		b, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			return err
-		}
-
-		body = bytes.NewReader(b)
-	}
-
-	var err error
-	switch service.SigningMethod {
-	case "v4", "s3v4":
-		_, err = p.Signer.Sign(req, body, service.SigningName, service.SigningRegion, time.Now())
-		break
-	case "s3":
-		_, err = p.Signer.Presign(req, body, service.SigningName, service.SigningRegion, time.Duration(time.Hour), time.Now())
-		break
-	default:
-		err = fmt.Errorf("unable to sign with specified signing method %s for service %s", service.SigningMethod, service.SigningName)
-		break
-	}
-
-	if err == nil {
-		log.WithFields(log.Fields{"service": service.SigningName, "region": service.SigningRegion}).Debug("signed request")
-	}
-
-	return err
+	return nil
 }
 
 func copyHeaderWithoutOverwrite(dst, src http.Header) {
